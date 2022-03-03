824f8ac403e81954f356916df75d0f72f116c1de
Dmuskeeter <tdheeraj9a@gmail.com>
2022-03-03 22:45:50 +0530

    secondry prompt file

---
+++ sec_prompt.py
@@ -0,0 +1,12 @@
+def add2num(x, y):
+    """Adds two given numbers and returns the sum.
+    >>> add2num(6, 7)
+    13
+    >>> add2num(-8.5, 7)
+    -2.5
+    """
+    return x + y
+
+
+
+    # python -m doctest sec_prompt.py
